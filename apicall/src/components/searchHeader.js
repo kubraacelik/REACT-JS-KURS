89015a9cd61ae4632d0109370ca1625b89a92edc
kubraacelik <kkbra.celik92@gmail.com>
2023-10-29 17:50:39 +0300

    added searchHeader component

---
+++ apicall/src/components/searchHeader.js
@@ -0,0 +1,12 @@
+function SearchHeader() {
+  return (
+    <div className="searchDiv">
+      <form>
+        <label>Ne Arıyorsunuz?</label>
+        <input type="text" />
+      </form>
+    </div>
+  );
+}
+
+export default SearchHeader;
